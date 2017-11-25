414ba4686ba66c01a8bd97c6f4320c67a71abc44
Emre Çelikten <emre@celikten.name>
2017-11-25 17:23:56 +0200

    Fix artist fetcher

--- KaggleDatasetCode/spotify_fetch.py
+++ KaggleDatasetCode/spotify_fetch.py
@@ -10,11 +10,18 @@ CREDENTIALS_PATH = "credentials.json"
 SIMILAR_ARTISTS_URL = "https://api.spotify.com/v1/artists/{id}/related-artists"
 
 
-def fetch_similar_artists(sp, artist_name):
+def fetch_artist_id(sp, artist_name):
     try:
         search_results = sp.search(artist_name, limit=3, type="artist")
         artist_id = search_results["artists"]["items"][0]["id"]
 
+        return artist_id
+    except:
+        return None
+
+
+def fetch_similar_artists(sp, artist_id):
+    try:
         relevant_artists_json = sp.artist_related_artists(artist_id)
 
         return [(artist["name"], artist["id"]) for artist in relevant_artists_json["artists"]]
@@ -22,24 +29,14 @@ def fetch_similar_artists(sp, artist_name):
         return None
 
 
-
-def construct_artist_graph(sp, unique_artist_set):
-    i = 0
+def construct_artist_graph(edge_tuples):
     edges = defaultdict(lambda: [])
 
-    for artist in unique_artist_set:
-        similar_artists = fetch_similar_artists(sp, artist)
+    for (a1, a2) in edge_tuples:
+        edges[a1].append(a2)
+        edges[a2].append(a1)
 
-        if not similar_artists: continue
-
-        for similar_artist in similar_artists:
-
-            edges[similar_artist[0]].append(artist)
-            edges[artist].append(similar_artist[0])
-
-        i += 1
-        if i % 100 == 0: print("At line {}".format(i))
-        #if i == 3: return edges
+    return edges
 
 
 def render_graph(edges):
@@ -55,15 +52,36 @@ def render_graph(edges):
     dot.render('test-output/artists', view=True)
 
 
-def main():
-    with open(CREDENTIALS_PATH) as f:
-        credentials_json = json.load(f)
+def phase1(sp):
+    unique_artists = load_unique_artist_list()
 
-    client_credentials_manager = SpotifyClientCredentials(client_id=credentials_json["CLIENT_ID"], client_secret=credentials_json["CLIENT_SECRET"])
+    tuple_list = []
+    i = 0
 
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    with open("similar.txt", "w") as f:
+        for artist in unique_artists:
+            try:
+                artist_id = fetch_artist_id(sp, artist)
 
-    graph = construct_artist_graph(sp, load_unique_artist_list())
+                if not artist_id: continue
+
+                similar_artists = fetch_similar_artists(sp, artist_id)
+
+                if not similar_artists: continue
+
+                for similar_artist in similar_artists:
+                    tuple_list.append((artist, similar_artist))
+                    print("{}<SEP>{}<SEP>{}<SEP>{}".format(artist_id, similar_artist[1], artist, similar_artist[0]), file=f, flush=True)
+
+                i += 1
+                if i % 250 == 0: print("At {}".format(i))
+            except:
+                print("Failed for artist: {}".format(artist))
+                continue
+
+
+def phase2(tuple_list):
+    graph = construct_artist_graph(tuple_list)
 
     graph = dict(graph)
 
@@ -73,6 +91,15 @@ def main():
     render_graph(graph)
 
 
+def main():
+    with open(CREDENTIALS_PATH) as f:
+        credentials_json = json.load(f)
+
+    client_credentials_manager = SpotifyClientCredentials(client_id=credentials_json["CLIENT_ID"], client_secret=credentials_json["CLIENT_SECRET"])
+
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    phase1(sp)
 
 
 if __name__ == "__main__":
